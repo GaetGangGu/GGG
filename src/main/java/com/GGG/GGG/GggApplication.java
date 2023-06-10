4ccbf8348ebe50d864424f047db553428934a89e
unknown <dlalsrl234@naver.com>
2023-06-10 10:47:28 +0900

    초기버전

---
+++ src/main/java/com/GGG/GGG/GggApplication.java
@@ -0,0 +1,13 @@
+package com.GGG.GGG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GGG/GGG/GggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GGG.GGG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
